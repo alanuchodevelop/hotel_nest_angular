87687a6e67b7da24c9ea1ac5301a6d957f71ea0e
Jesus Aguilar <efrain.duarte@infotec.mx>
2024-09-26 18:03:34 -0600

    Feature: * add extra files (untracking)

---
+++ client/src/app/rooms/rooms.component.ts
@@ -0,0 +1,23 @@
+import {Component, inject} from '@angular/core';
+import { RoomsService } from '../services/rooms.service';
+
+@Component({
+  selector: 'app-rooms',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css'
+})
+export class RoomsComponent {
+
+  rooms: any[] = [];
+  // Inyectar el servicio directamente con el método inject
+  private roomsService = inject(RoomsService);
+
+
+  ngOnInit(): void {
+    this.roomsService.getRooms().subscribe((data) => {
+      this.rooms = data;
+    });
+  }
+}
